41b5df6eb1ce4ccce46d163db80c8a52b37f7cd6
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2021-06-09 12:36:16 +0300

    Добавлен плагин для очищения каталога dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
-const {src, dest} = require('gulp') 
+const {src, dest, task} = require('gulp')
+const rm = require( 'gulp-rm' ) 
 
 const file = [
   'src/styles/*.scss',
   '!src/styles/two.scss'
 ]
+
+task( 'clean', () => {
+  return src( 'dist/**/*', { read: false })
+    .pipe( rm() )
+})
 
 function copy() {
   return src(file).pipe(dest('dist'))
