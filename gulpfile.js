11be71da80dc1b76b40d829fca177d7b2865a763
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2021-06-10 09:02:52 +0300

    Добавлен gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 const {src, dest, task, series} = require('gulp')
 const rm = require( 'gulp-rm' ) 
-
-const file = [
-  'src/styles/*.scss',
-  '!src/styles/two.scss'
-]
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 task( 'clean', () => {
   return src( 'dist/**/*', { read: false })
     .pipe( rm() )
 })
 
-task( 'copy', () =>  {
-  return src(file).pipe(dest('dist'))}
-)
+task('styles', () => {
+  return src('src/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist'));
+});
 
-task('default', series('clean', 'copy'))
+task('default', series('clean', 'styles'))
