2301d58e7d6b50a0e7c4f634081c8a6a2d5f644c
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2021-06-10 09:15:12 +0300

    Склейка файлов. Подключение gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,21 @@ const {src, dest, task, series} = require('gulp')
 const rm = require( 'gulp-rm' ) 
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const concat = require('gulp-concat');
 
 task( 'clean', () => {
   return src( 'dist/**/*', { read: false })
     .pipe( rm() )
 })
 
+const styles = [
+  'node_modules/normalize.css/normalize.css',
+  'src/styles/main.scss'
+]
+
 task('styles', () => {
-  return src('src/styles/main.scss')
+  return src(styles)
+    .pipe(concat('main.scss'))
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('dist'));
 });
