dda09a219e750ac6e091791ae4b00d10f3d498c5
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2021-06-10 09:23:01 +0300

    Слежка за файлами. Watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
-const {src, dest, task, series} = require('gulp')
-const rm = require( 'gulp-rm' ) 
-const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
-const concat = require('gulp-concat');
+const { src, dest, task, series, watch } = require("gulp");
+const rm = require("gulp-rm");
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+const concat = require("gulp-concat");
 
-task( 'clean', () => {
-  return src( 'dist/**/*', { read: false })
-    .pipe( rm() )
-})
-
-const styles = [
-  'node_modules/normalize.css/normalize.css',
-  'src/styles/main.scss'
-]
-
-task('styles', () => {
-  return src(styles)
-    .pipe(concat('main.scss'))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('dist'));
+task("clean", () => {
+  return src("dist/**/*", { read: false }).pipe(rm());
 });
 
-task('default', series('clean', 'styles'))
+const styles = [
+  "node_modules/normalize.css/normalize.css",
+  "src/styles/main.scss",
+];
+
+task("styles", () => {
+  return src(styles)
+    .pipe(concat("main.scss"))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("dist"));
+});
+
+watch("src/styles/**/*.scss", series("styles"));
+
+task("default", series("clean", "styles"));
